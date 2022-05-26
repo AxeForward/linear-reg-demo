430893838f56650653fb0d1a5edbc6e247e7dd95
史昀辉 <yunhui.shi@inboc.net>
2022-05-26 11:20:31 +0800

    Function to calculate linear regression coefficients. Add test functions.

--- Code/setup.py
+++ Code/setup.py
@@ -1,2 +1,31 @@
-import math
-print("Hello Axe Forward!")
+import numpy as np
+import pandas as pd
+import os
+
+class irreversible(Exception):
+    """
+    Pop up
+    """
+
+def calc_coef(arr_x, arr_y):
+    XX = np.matmul(arr_x.T, arr_x)
+    if np.linalg.det(XX) == 0:
+        raise irreversible('Matrix Irreversible')
+    else:
+        inv_XX = np.linalg.inv(XX)
+        temp = np.matmul(inv_XX, arr_x.T)
+        arr_coef = np.matmul(temp, arr_y)
+        return arr_coef
+
+data_path = '../Data/calc_data.xlsx'
+df = pd.read_excel(data_path, index_col=0, parse_dates=['Date'])
+df['const'] = 1
+y = df.IdxMonRet.values
+X = df[['Rmrf_mc', 'Smb_mc', 'Hml_mc', 'const']].values
+
+beta = calc_coef(X, y)
+print("alpha = ", beta[3])
+
+
+
+
--- Code/test_setup.py
+++ Code/test_setup.py
@@ -1,1 +1,16 @@
 import pytest
+import numpy as np
+import pandas as pd
+from setup import calc_coef, irreversible
+
+def test_func1():
+    test_x = np.array([[1, 2], [3, 4]])
+    test_y = np.array([5, 6])
+    beta = calc_coef(test_x, test_y)
+    return beta
+
+def test_func2():
+    test_x = np.array([[1, 0], [1, 0]])
+    test_y = np.array([2, 3])
+    with pytest.raises(irreversible):
+        beta = calc_coef(test_x, test_y)
